d2422eeb67a51f26bb2063b43716afaa7e7b396a
Dharko <99974989+Dharko@users.noreply.github.com>
2022-06-10 23:05:51 -0500

    Add files via upload

---
+++ sopa/jvs.js
@@ -0,0 +1,305 @@
+var a1, a2, a3, a4, a5, a6, a7, a8, a9, a10,
+a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25,a26,a27,a28,a29,
+a30,a31,a32,a33,a34,a35,a36,a37,a38,a39,a40,a41,a42,a43,a44,a45,a46,a47,a48,a49,a50,a51,a52,a53,a54,a55,
+a56,a57,a58,a59,a60,a61,a62,a63,a64,a65,a66,a67,a68,a69,a70,a71,a72,a73,a74,a75,a76,a77,a78,a79,a80,a81,
+a82,a83,a84,a85,a86,a87,a88,a89,a90,a91,a92,a93,a94,a95,a96,a97
+
+
+
+//palabra JavaScript color rojo
+function comprobar1() {
+    a1 = document.getElementById('celda1').style.color = "red";
+}
+
+function comprobar2() {
+    a2 = document.getElementById('celda2').style.color = "red";
+}
+function comprobar3() {
+    a3 = document.getElementById('celda3').style.color = "red";
+}
+function comprobar4() {
+    a4 = document.getElementById('celda4').style.color = "red";
+}
+
+function comprobar5() {
+    a5 = document.getElementById('celda5').style.color = "red";
+}
+
+function comprobar6() {
+    a6 = document.getElementById('celda6').style.color = "red";
+}
+
+function comprobar7() {
+    a7 = document.getElementById('celda7').style.color = "red";
+}
+function comprobar8() {
+    a8 = document.getElementById('celda8').style.color = "red";
+}
+function comprobar9() {
+    a9 = document.getElementById('celda9').style.color = "red";
+}
+function comprobar10() {
+    a10 = document.getElementById('celda10').style.color = "red";
+}
+function comprobar11() {
+    a11 = document.getElementById('celda11').style.color = "red";
+}
+function comprobar12() {
+    a12 = document.getElementById('celda12').style.color = "red";
+}
+function comprobar13() {
+    a13 = document.getElementById('celda13').style.color = "red";
+}
+function comprobar14() {
+    a14 = document.getElementById('celda14').style.color = "red";
+}
+function comprobar15() {
+    a15 = document.getElementById('celda15').style.color = "red";
+}
+function comprobar16() {
+    a16 = document.getElementById('celda16').style.color = "red";
+}
+function comprobar17() {
+    a17 = document.getElementById('celda17').style.color = "red";
+}
+function comprobar18() {
+    a18 = document.getElementById('celda18').style.color = "red";
+}
+function comprobar19() {
+    a19 = document.getElementById('celda19').style.color = "red";
+}
+function comprobar20() {
+    a20 = document.getElementById('celda20').style.color = "red";
+}
+function comprobar21() {
+    a21 = document.getElementById('celda21').style.color = "red";
+}
+function comprobar22() {
+    a22 = document.getElementById('celda22').style.color = "red";
+}
+function comprobar23() {
+    a23 = document.getElementById('celda23').style.color = "red";
+}
+function comprobar24() {
+    a24 = document.getElementById('celda24').style.color = "red";
+}
+function comprobar25() {
+    a25 = document.getElementById('celda25').style.color = "red";
+}
+function comprobar26() {
+    a26 = document.getElementById('celda26').style.color = "red";
+}
+function comprobar27() {
+    a27 = document.getElementById('celda27').style.color = "red";
+}
+function comprobar28() {
+    a28 = document.getElementById('celda28').style.color = "red";
+}
+function comprobar29() {
+    a29 = document.getElementById('celda29').style.color = "red";
+}
+function comprobar30() {
+    a30 = document.getElementById('celda30').style.color = "red";
+}
+function comprobar31() {
+    a31 = document.getElementById('celda31').style.color = "red";
+}
+function comprobar32() {
+    a32 = document.getElementById('celda32').style.color = "red";
+}
+function comprobar33() {
+    a33 = document.getElementById('celda33').style.color = "red";
+}
+function comprobar34() {
+    a34 = document.getElementById('celda34').style.color = "red";
+}
+function comprobar35() {
+    a35 = document.getElementById('celda35').style.color = "red";
+}
+function comprobar36() {
+    a36 = document.getElementById('celda36').style.color = "red";
+}
+function comprobar37() {
+    a37 = document.getElementById('celda37').style.color = "red";
+}
+function comprobar38() {
+    a38 = document.getElementById('celda38').style.color = "red";
+}
+function comprobar39() {
+    a39 = document.getElementById('celda39').style.color = "red";
+}
+function comprobar40() {
+    a40 = document.getElementById('celda40').style.color = "red";
+}
+function comprobar41() {
+    a41 = document.getElementById('celda41').style.color = "red";
+}
+function comprobar42() {
+    a42 = document.getElementById('celda42').style.color = "red";
+}
+function comprobar43() {
+    a43 = document.getElementById('celda43').style.color = "red";
+}
+function comprobar44() {
+    a44 = document.getElementById('celda44').style.color = "red";
+}
+function comprobar45() {
+    a45 = document.getElementById('celda45').style.color = "red";
+}
+function comprobar46() {
+    a46 = document.getElementById('celda46').style.color = "red";
+}
+function comprobar47() {
+    a47 = document.getElementById('celda47').style.color = "red";
+}
+function comprobar48() {
+    a48 = document.getElementById('celda48').style.color = "red";
+}
+function comprobar49() {
+    a49 = document.getElementById('celda49').style.color = "red";
+}
+function comprobar50() {
+    a50 = document.getElementById('celda50').style.color = "red";
+}
+function comprobar51() {
+    a51 = document.getElementById('celda51').style.color = "red";
+}
+function comprobar52() {
+    a52 = document.getElementById('celda52').style.color = "red";
+}
+function comprobar53() {
+    a53 = document.getElementById('celda53').style.color = "red";
+}
+function comprobar54() {
+    a54 = document.getElementById('celda54').style.color = "red";
+}
+function comprobar55() {
+    a55 = document.getElementById('celda55').style.color = "red";
+}
+function comprobar56() {
+    a56 = document.getElementById('celda56').style.color = "red";
+}
+function comprobar56() {
+    a57 = document.getElementById('celda57').style.color = "red";
+}
+function comprobar58() {
+    a58 = document.getElementById('celda58').style.color = "red";
+}
+function comprobar59() {
+    a59 = document.getElementById('celda59').style.color = "red";
+}
+function comprobar60() {
+    a60 = document.getElementById('celda60').style.color = "red";
+}
+function comprobar61() {
+    a61 = document.getElementById('celda61').style.color = "red";
+}
+function comprobar62() {
+    a62 = document.getElementById('celda62').style.color = "red";
+}
+function comprobar63() {
+    a63 = document.getElementById('celda63').style.color = "red";
+}
+function comprobar64() {
+    a64 = document.getElementById('celda64').style.color = "red";
+}
+function comprobar65() {
+    a65 = document.getElementById('celda65').style.color = "red";
+}
+function comprobar66() {
+    a66 = document.getElementById('celda66').style.color = "red";
+}
+function comprobar67() {
+    a67 = document.getElementById('celda67').style.color = "red";
+}
+function comprobar68() {
+    a68 = document.getElementById('celda68').style.color = "red";
+}
+function comprobar69() {
+    a69 = document.getElementById('celda69').style.color = "red";
+}
+function comprobar70() {
+    a70 = document.getElementById('celda70').style.color = "red";
+}
+function comprobar71() {
+    a71 = document.getElementById('celda71').style.color = "red";
+}
+function comprobar72() {
+    a72 = document.getElementById('celda72').style.color = "red";
+}
+function comprobar73() {
+    a73 = document.getElementById('celda73').style.color = "red";
+}
+function comprobar74() {
+    a74 = document.getElementById('celda74').style.color = "red";
+}
+function comprobar75() {
+    a75 = document.getElementById('celda75').style.color = "red";
+}
+function comprobar76() {
+    a76 = document.getElementById('celda76').style.color = "red";
+}
+function comprobar77() {
+    a77 = document.getElementById('celda77').style.color = "red";
+}
+function comprobar78() {
+    a78 = document.getElementById('celda78').style.color = "red";
+}
+function comprobar79() {
+    a79 = document.getElementById('celda79').style.color = "red";
+}
+function comprobar80() {
+    a80 = document.getElementById('celda80').style.color = "red";
+}
+function comprobar81() {
+    a81 = document.getElementById('celda81').style.color = "red";
+}
+function comprobar82() {
+    a82 = document.getElementById('celda82').style.color = "red";
+}
+function comprobar83() {
+    a83 = document.getElementById('celda83').style.color = "red";
+}
+function comprobar84() {
+    a84 = document.getElementById('celda84').style.color = "red";
+}
+function comprobar85() {
+    a85 = document.getElementById('celda85').style.color = "red";
+}
+function comprobar86() {
+    a86 = document.getElementById('celda86').style.color = "red";
+}
+function comprobar87() {
+    a87 = document.getElementById('celda87').style.color = "red";
+}
+function comprobar88() {
+    a88 = document.getElementById('celda88').style.color = "red";
+}
+function comprobar89() {
+    a89 = document.getElementById('celda89').style.color = "red";
+}
+function comprobar90() {
+    a90 = document.getElementById('celda90').style.color = "red";
+}
+function comprobar91() {
+    a91 = document.getElementById('celda91').style.color = "red";
+}
+function comprobar92() {
+    a92 = document.getElementById('celda92').style.color = "red";
+}
+function comprobar93() {
+    a93 = document.getElementById('celda93').style.color = "red";
+}
+function comprobar94() {
+    a94 = document.getElementById('celda94').style.color = "red";
+}
+function comprobar95() {
+    a95 = document.getElementById('celda95').style.color = "red";
+}
+function comprobar96() {
+    a96 = document.getElementById('celda96').style.color = "red";
+}
+function comprobar97() {
+    a97 = document.getElementById('celda97').style.color = "red";
+}
+
